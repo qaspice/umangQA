3937f1c47c1992e049dcf1b79f1684b10449e17b
gurkirat <gurkirat.singh@digispice.com>
2018-12-18 17:10:10 +0530

    sabsad

---
+++ Test.java
@@ -0,0 +1,87 @@
+
+import java.util.*;
+
+public class Test
+	
+{
+
+/*public static void test(){
+System.out.println("test");
+}
+public static int test(){
+
+return 2;
+}
+
+public static byte test(){
+
+return 1;
+}
+public static float test(){
+
+return 15.15;
+}*/
+
+/*public static int add(int a, int b)
+{
+
+
+System.out.println("add method with two arguments: "+a+" and "+b);
+
+
+return a+b;
+}
+
+
+public static void main(String...s)
+{
+
+
+add(1 ,2);
+
+
+}*/
+	/*public static void test(int x){
+	System.out.println("integer"+x);
+}
+		public static void test(short x)
+{
+	System.out.println("short"+x);
+}
+
+		public static void test (long x )
+{
+	System.out.println("long"+x);
+
+}
+
+		public static void test(byte x)
+{
+		System.out.println("byte"+x);
+}
+public static void main(String...s)
+{
+	test(1);
+}*/
+
+		public static void main (String args[])
+{
+			char branch = 'c';
+			int collegeyear = 4;
+			switch(collegeYear)
+{
+		case 1: 
+			System.out.println("Maths,English,Hindi");
+		break;
+}
+
+		case 2:
+			switch(branch)
+{
+
+		case 'c':
+			System.out.println("x,y,z");
+	break;
+}
+}
+}
---
+++ umang/src/main/java/com/spice/dataReader/TestingGit.java
@@ -0,0 +1,5 @@
+package com.spice.dataReader;
+
+public class TestingGit {
+
+}
